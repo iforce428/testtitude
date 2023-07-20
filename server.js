06aaef80efed0138f94b3cd556cc2dbbe02e8dd0
amirsufi <amirsufi532@gmail.com>
2023-07-20 18:32:54 +0800

    data push

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function (req, res) {
-    res.render('pages/index');
+app.get('/', function(req, res) {
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 // about page
-app.get('/about', function (req, res) {
-    res.render('pages/about');
+app.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
 app.listen(8080);
